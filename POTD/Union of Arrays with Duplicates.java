bf50a6da9c68c435f52bc4f310a422cbd5aa60fc
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2024-12-30 11:23:13 +0530

    Create Union of Arrays with Duplicates.java

---
+++ POTD/Union of Arrays with Duplicates.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int findUnion(int a[], int b[]) {
+        // code here
+        HashSet<Integer> set=new HashSet<>();
+    
+        for(int i=0;i<a.length;i++){
+            set.add(a[i]);
+        }
+        for(int i=0;i<b.length;i++){
+            set.add(b[i]);
+        }
+           return set.size();
+    }
+}
