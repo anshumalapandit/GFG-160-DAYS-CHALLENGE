ffd9c0e43975e69f282c94bc276f9e5ef4a10f08
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2024-12-31 15:26:47 +0530

    Update Longest Consecutive Subsequence.java

--- POTD/Longest Consecutive Subsequence.java
+++ POTD/Longest Consecutive Subsequence.java
@@ -25,3 +25,29 @@ class Solution {
         return longest;
     }
 }
+// better approach 0(nlogn)
+class Solution {
+    
+
+    // Function to return length of longest subsequence of consecutive integers.
+    public int longestConsecutive(int[] arr) {
+        // code here
+        // better force 
+        Arrays.sort(arr);
+        int longest=1;
+        int lastsmallest=Integer.MIN_VALUE;
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]-1==lastsmallest){
+                count++;
+                lastsmallest=arr[i];
+            }else if(lastsmallest!=arr[i]){
+               
+                 count=1;
+                 lastsmallest=arr[i];
+            }
+            longest=Math.max(count,longest);
+        }
+        return longest;
+    }
+}
