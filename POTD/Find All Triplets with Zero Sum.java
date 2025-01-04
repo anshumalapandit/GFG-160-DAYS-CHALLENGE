54418ff3b65b51d045714ef8eb74e6a397b43a0d
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2025-01-04 20:33:47 +0530

    Update Find All Triplets with Zero Sum.java

--- POTD/Find All Triplets with Zero Sum.java
+++ POTD/Find All Triplets with Zero Sum.java
@@ -20,3 +20,44 @@ class Solution {
         return ans;
     }
 }
+// optimised its is given that array is sorted then take its benefits use two pointer approach
+class Solution {
+    public int countTriplets(int[] arr, int target) {
+        // Code Here
+        int count=0;
+        int n=arr.length;
+        
+        for(int i=0;i<n-2;i++){
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                int sum=arr[i]+arr[left]+arr[right];
+                if(sum==target){
+                    count++;
+                   // Check all possible duplicates for the same target sum
+                    int tempLow = left+ 1;
+                    int tempHigh = right - 1;
+
+                    while (tempLow < right && arr[tempLow] == arr[left]) {
+                        count++;
+                        tempLow++;
+                    }
+                    while (tempHigh > left && arr[tempHigh] == arr[right]) {
+                        count++;
+                        tempHigh--;
+                    }
+
+                    // Move pointers inward
+                    left++;
+                    right--;
+                }
+                else if(sum<target){
+                    left++;
+                }else{
+                    right--;
+                }
+            }
+        }
+        return count;
+    }
+}
